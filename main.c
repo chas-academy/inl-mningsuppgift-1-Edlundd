557dc50d380e56e6101a26326ca1ff2b54ebd8e4
medlund01@gmail.com <medlund01@gmail.com>
2025-04-24 09:51:31 +0200

    Dice rolls working

--- main.c
+++ main.c
@@ -8,6 +8,27 @@ int main()
 
    
     // Your code goes here
+    
+    int dice_rolls[100];                     // Array for 100 rolls
+
+
+    // Randomize 100 dicerolls and stores in array dice_sum
+    for (int i = 0; i < 100; i++){        
+        dice_rolls[i] = (rand() % 6) + 1;  // Gives random number 1-6
+        printf("roll %d\n", dice_rolls[i]);
+        }
+    
+
+    // Print number of sides (1-6)
+    
+                  // Print number of 1s, 2s, 3s etc..
+    
+    
+    // Print sum of all roll
+    
+    // Calculate average value of all rolls and print with 1 decimal
+              // Divide sum with 100 to get average value
+                // Print average value
 
     return 0;
 }
